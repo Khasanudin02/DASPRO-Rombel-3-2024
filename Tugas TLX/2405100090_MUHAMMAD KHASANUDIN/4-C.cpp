59d5851ca60ec9bce4fb9cae004b7f7340bf99f2
Khasanudin02 <muhammadkhasanudin02@gmail.com>
2024-10-10 20:42:10 +0700

    Update 4-C.cpp
    logika else if

---
+++ Tugas TLX/2405100090_MUHAMMAD KHASANUDIN/4-C.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total_bebek = 67 + 98;
+
+    if (total_bebek % 2 == 0) { //total bebek dibagi 2
+        cout << "total banyaknya bebek adalah bilangan genap" << endl;
+    } else {
+        cout << "total banyaknya bebek adalah bilangan ganjil" << endl;
+    }
+//jika hasilnya genap = 0, jika salah hasilnya = 1
+
+    
+}
