1b6e73f60a27583a5ceb5f0d6027760b6944702b
Khasanudin02 <muhammadkhasanudin02@gmail.com>
2024-10-11 09:03:01 +0700

    Update 10-A.cpp
    Untuk memastikan bahwa Anda telah berhasil menginstal g++ dengan benar, bukalah sebuah terminal/shell

---
+++ Tugas TLX/2405100090_MUHAMMAD KHASANUDIN/10-A.cpp
@@ -0,0 +1,12 @@
+Untuk memastikan bahwa Anda telah berhasil menginstal g++ dengan benar, bukalah sebuah terminal/shell, lalu ketikkan perintah berikut:
+
+g++ --version
+Tekan Enter. Perintah tersebut akan mengeluarkan sejumlah baris yang mirip seperti berikut:
+
+g++ …
+Copyright (C) …
+This is free software; …
+Pastikan Anda mendapat keluaran seperti di atas!
+
+  maka keluarannya seperti ini
+g++ (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0
